71cbc48da9c4a87dca00a5bf4458519cea812d45
michael hohl <mhohl@rocketmail.com>
2018-04-17 21:31:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each { |k,v| self.send("#{attribute}=", value) }
+    student_hash.each do { |k,v| self.send("#{attribute}=", value) }
     @@all << self
   end
 
